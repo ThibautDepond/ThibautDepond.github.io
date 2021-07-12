4b04fce248427105903c120548d540d96e309acc
ThibautDepond-Git <57394745+ThibautDepond-Git@users.noreply.github.com>
2021-07-12 22:01:48 +0200

    Update index.js

--- Plugin/index.js
+++ Plugin/index.js
@@ -1,5 +1,1 @@
-import { hey, heyWithAsyncAwait} from './fetchExemple.js'
-
 import { buildheader } from './autoload.js'
-
-import { fetchItunesSongs, handleClickSong, createSongLI, search } from './ituneAPI.js'
